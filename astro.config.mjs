ac275c020e2c686729bb69511c6014a52c731696
achristiana36 <anthonyjchristiana@gmail.com>
2026-08-16 11:48:48 -0400

    Configure antchristiana.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-	site: 'https://achristiana36.github.io',
-	base: '/antchristiana-site',
+  site: 'https://antchristiana.com',
 });
